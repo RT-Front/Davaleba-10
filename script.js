c5039468dccbbbaceb2755cb83d442afbf7c86fc
RT-Front <109627649+RT-Front@users.noreply.github.com>
2022-09-04 18:28:14 +0400

    second commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const personalInfo = {
+    firstName: 'Reshad',
+    lastName: 'Tsetskhladze',
+    age: 22,
+    marriage: 'trapped',
+    job: 'gamer :D, for now!',
+    kills: 'alot',
+    death: 'zero death',
+}
+
+
+
+
+console.log(personalInfo)
